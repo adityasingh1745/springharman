53e17c6ccc0154f14a3d17f49171f127857d84d0
ASingh1 <ASingh1@INBAWN166769.ad.harman.com>
2022-04-25 12:18:53 +0530

    Constructor Injection

--- springharman/src/springharman/model/Test.java
+++ springharman/src/springharman/model/Test.java
@@ -7,28 +7,15 @@ import org.springframework.core.io.Resource;
 
 public class Test {  
 	public static void main(String[] args) {  
-		 
-		
-		
-		for(int i=0; i<10; i++) {
-			Resource resource=new ClassPathResource("applicationContext.xml");
-			//applicationContext.xml is my configuration file
-			BeanFactory factory=new XmlBeanFactory(resource); 
+		Resource resource=new ClassPathResource("applicationContext.xml");
+		//applicationContext.xml is my configuration file
+		BeanFactory factory=new XmlBeanFactory(resource);  
 
+		Student student = (Student)factory.getBean("studentbean");  
+		Employee employee = (Employee) factory.getBean("emp");
 		//im not instantiating a Student object -- new Student()
 		//instead the spring is instantiating it from me and injecting it
-		
-			
-			try {
-				Thread.sleep(2000);
-				Student student = (Student)factory.getBean("studentbean");  
-
-				student.displayInfo();  
-
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		employee.show();
+		//student.displayInfo();  
 	}  
-}
+}  
